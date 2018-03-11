537f56188f4c8202524aad9ec73a08991b3188a4
assisken <assisken@gmail.com>
2018-03-11 17:46:13 +0300

    Specs for statements.

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -9,19 +9,77 @@ module Igarry
       'Test command...'
     end
   end
+  module NoneCommand
+    extend CommandContainer
+    command :none, state: State::NONE do
+      'Hi!'
+    end
+  end
+  module AddingCommand
+    extend CommandContainer
+    command :adding, state: State::ADDING do
+      'I\'m grout!'
+    end
+  end
 
   describe Bot do
     let(:bot) { Bot.new }
+    let(:message) do
+      message = Telegram::Bot::Types::Message.new
+      message.from = Telegram::Bot::Types::User.new
+      message.chat = Telegram::Bot::Types::Chat.new
+      message
+    end
 
     describe '.include!' do
       it 'adds command to controller' do
         bot.include! TestCommand
-        message = Telegram::Bot::Types::Message.new
-        from = Telegram::Bot::Types::User.new
-        from.id = 0
-        message.from = from
-        message.text = '/test'
-        expect(bot.execute(bot, message)).to eq('Test command...')
+        mess = message.clone
+        mess.from.id = 0
+        mess.text = '/test'
+        expect(bot.execute(bot, mess)).to eq('Test command...')
+      end
+    end
+
+    describe 'state' do
+      context 'none command' do
+        subject(:mess) do
+          mess = message.clone
+          mess.from.id = 0
+          mess.text = '/none'
+          bot.include! NoneCommand
+          mess
+        end
+
+        it('calls normally') do
+          expect(bot.execute(nil, mess)).to eq('Hi!')
+        end
+
+        it('raises error at wrong state') do
+          bot.add_state State::ADDING, 0
+          expect(bot.execute(nil, mess)).to be_instance_of RuntimeError
+          bot.remove_state 0
+        end
+      end
+
+      context 'adding command' do
+        subject(:mess) do
+          mess = message.clone
+          mess.from.id = 0
+          mess.text = '/adding'
+          bot.include! AddingCommand
+          mess
+        end
+
+        it('calls normally') do
+          bot.add_state State::ADDING, 0
+          expect(bot.execute(nil, mess)).to eq('I\'m grout!')
+          bot.remove_state 0
+        end
+
+        it('raises error at wrong state') do
+          expect(bot.execute(nil, mess)).to be_instance_of RuntimeError
+        end
       end
     end
   end
--- spec/command_controller_spec.rb
+++ spec/command_controller_spec.rb
@@ -4,13 +4,28 @@ require 'logger'
 require 'telegram/bot'
 
 LOGGER = Logger.new nil
+LOGGER.level = Logger::ERROR
 
 module Igarry
   describe CommandController do
     let(:controller)    { Igarry::CommandController.new }
     let(:user_command)  { Command.new(:test, permissions: [:ALL]) { 'Hello there!' } }
     let(:admin_command) { Command.new(:test_admin, permissions: [:ADMIN]) { 'I\'m test command' } }
+    let(:user_message) do
+      message = Telegram::Bot::Types::Message.new
+      message.from = Telegram::Bot::Types::User.new
+      message.from.id = 0
+      message.text = '/test'
+      message
+    end
+    let(:admin_message) do
+      admin = user_message.clone
+      admin.from.id = 1
+      admin.text = '/test_admin'
+      admin
+    end
     before do
+      controller.instance_variable_set :@users, ADMIN: [1]
       controller.add user_command
       controller.add admin_command
     end
@@ -23,32 +38,38 @@ module Igarry
       it('includes an admin command') { expect include admin_command.name }
     end
 
-    describe '.get_command' do
-      subject(:command) { controller.get_command('/test') }
-      subject(:wrong_command) { controller.get_command('test') }
+    describe '.call' do
+      subject(:wrong_message) do
+        wrong = user_message.clone
+        wrong.text = 'test'
+        wrong
+      end
 
-      it('transforms a string to a symbol') { expect(command.name).to be(:test) }
-      it('calls a command') { expect(command.call(nil, nil)).to eq('Hello there!') }
-
+      it('calls a command') { expect(controller.call(nil, user_message)).to eq('Hello there!') }
       it 'calls wrong command' do
-        expect { wrong_command.call(nil, nil) }
-          .to raise_error(NoMethodError, 'undefined method `call\' for nil:NilClass')
+        expect { controller.call(nil, wrong_message) }
+          .to raise_error(RuntimeError, 'No such command')
       end
     end
 
-    describe '.permission?' do
-      context 'admin' do
-        subject(:admin) { ENV['ADMIN'] }
+    describe 'permission' do
+      context 'for admin' do
+        subject(:admin) { 1 }
 
-        it('passes admin command') { expect(controller.permission?(user_command, admin)).to be_truthy }
-        it('passes user command') { expect(controller.permission?(admin_command, admin)).to be_truthy }
+        it('passes admin command') { expect(controller.call(nil, admin_message)).to eq('I\'m test command') }
+        it('passes user command')  { expect(controller.call(nil, admin_message)).to eq('I\'m test command') }
       end
 
-      context 'user' do
+      context 'for user' do
         subject(:user) { 0 }
+        subject(:user_message2) do
+          message = admin_message.clone
+          message.from.id = user
+          message
+        end
 
-        it('passes user command') { expect(controller.permission?(user_command, user)).to be_truthy }
-        it('does\'t pass admin command') { expect(controller.permission?(admin_command, user)).to be_falsey }
+        it('passes user command') { expect(controller.call(nil, user_message)).to eq('Hello there!') }
+        it('does\'t pass admin command') { expect(controller.call(nil, user_message2)).to be_instance_of RuntimeError }
       end
     end
   end
